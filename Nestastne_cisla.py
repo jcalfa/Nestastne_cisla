e683d6d4415f3eee2cfcc3fa9742f35e3ec75478
jcalfa <calfa.j@gmail.com>
2021-03-13 22:29:35 +0100

    Zmena codu

--- Nestastne_cisla.py
+++ Nestastne_cisla.py
@@ -1,7 +1,16 @@
+# for cislo in range (1,21):
+#     if cislo == 4 or cislo == 13:
+#         print(f"{cislo} Neparne")
+#     elif cislo % 2 == 0:
+#         print(f"{cislo} Parne")
+#     else :
+#         print(f"{cislo} Neparne")
+
 for cislo in range (1,21):
     if cislo == 4 or cislo == 13:
-        print(f"{cislo} Neparne")
+        state = "Nestastne"
     elif cislo % 2 == 0:
-        print(f"{cislo} Parne")
-    else :
-        print(f"{cislo} Neparne")
+        state = "Parne"
+    else:
+        state = "Neparne"
+    print(f"{cislo} je {state}")
