8c7bc12377e06633d013a773f00ce40befe7f221
jcalfa <calfa.j@gmail.com>
2021-03-24 20:36:26 +0100

    Emoji programek

---
+++ art.py
@@ -0,0 +1,9 @@
+for i in range(3):
+    for cislo in range (1,11):
+        print("\U0001f600" * cislo) 
+
+# pocet = 1
+# while pocet < 11:
+#     print("\U0001f600" * pocet)
+#     pocet += 1
+    
