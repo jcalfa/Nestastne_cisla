cde8bea1b7dad5f8a1372d0a1b7aa41a6cffce61
jcalfa <calfa.j@gmail.com>
2021-03-24 16:58:32 +0100

    Pridany While_loop program

---
+++ While_loop-Example.py
@@ -0,0 +1,7 @@
+# While Loop - Example
+
+Sprava = input("Ake je tajne heslo?")
+while Sprava != "Banany":
+    print("ZLE HESLO !!!")
+    Sprava = input("Ake je tajne heslo?")
+print("SPRAVNE HESLO !!!")
