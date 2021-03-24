0ccd9aebe0f0ac7d7a8ea811415eb46a6f7a0ce1
jcalfa <calfa.j@gmail.com>
2021-03-24 17:20:01 +0100

    SPRAVNE HESLO

--- While_loop-Example.py
+++ While_loop-Example.py
@@ -4,4 +4,4 @@ Sprava = input("Ake je tajne heslo?")
 while Sprava != "Banany":
     print("ZLE HESLO !!!")
     Sprava = input("Ake je tajne heslo?")
-print("SPRAVNE HESLO - Pustim ta dnu !!!")
+print("SPRAVNE HESLO")
