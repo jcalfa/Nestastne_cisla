f7bcdcef494a3cb0e927654a0426daa2d053fc90
jcalfa <calfa.j@gmail.com>
2021-03-24 19:10:50 +0100

    While loop example - increment from 1 to 10

--- While_loop-Example.py
+++ While_loop-Example.py
@@ -1,7 +1,15 @@
 # While Loop - Example
 
-Sprava = input("Ake je tajne heslo?")
-while Sprava != "Banany":
-    print("ZLE HESLO !!!")
-    Sprava = input("Ake je tajne heslo?")
-print("SPRAVNE HESLO - Updated!!!")
+# Sprava = input("Ake je tajne heslo?")
+# while Sprava != "Banany":
+#     print("ZLE HESLO !!!")
+#     Sprava = input("Ake je tajne heslo?")
+# print("SPRAVNE HESLO - Updated!!!")
+
+# for cislo in range(1,11):
+#     print(cislo)
+
+cislo = 1
+while cislo < 11:
+    print(cislo)
+    cislo += 1
