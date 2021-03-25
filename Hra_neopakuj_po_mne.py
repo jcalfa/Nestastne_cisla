008f9bd8bc1b37d5b49bbef059fd20c6e8d0fbe3
jcalfa <calfa.j@gmail.com>
2021-03-25 15:09:45 +0100

    Novy programcek - Hra Neopakuj po mne

---
+++ Hra_neopakuj_po_mne.py
@@ -0,0 +1,12 @@
+# Stop copying me game - program zopakuje vsetko co do neho napises!!!
+#
+sprava = input("Povedz nieco:")
+
+# while sprava != "neopakuj po mne":
+#     print(sprava)
+#     sprava = input()
+# print("Ok vyhral si!")
+
+while sprava != "neopakuj po mne":
+    sprava = input(f"{sprava}\n")
+print("Ok vyhral si!")
