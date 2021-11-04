45430efbde49cbd0c1dc8eb475658f10a40e53ff
jcalfa <calfa.j@gmail.com>
2021-11-04 17:57:19 +0100

    Added

---
+++ Upper_Case.py
@@ -0,0 +1,11 @@
+# def yell(word):
+#     return f"{word.upper()}!"
+# print(yell("go away"))
+
+# def yell(word):
+#     return word.upper() + "!"
+# print(yell("go away"))
+
+def yell(word):
+    return "{}!".format(word.upper())
+print(yell("go away"))
