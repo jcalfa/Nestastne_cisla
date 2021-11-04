61af717213958a20c29fb00fba1623d09fca3d71
jcalfa <calfa.j@gmail.com>
2021-11-04 17:12:26 +0100

    Generate even numbers

---
+++ Generate_Events_v2.py
@@ -0,0 +1,7 @@
+def generate_evens():
+    result = []
+    for x in range(1,50):
+        if x % 2 == 0:
+            result.append(x)
+    return result
+print(generate_evens())
---
+++ Generating_Evens.py
@@ -0,0 +1,3 @@
+def generate_evens():
+    return [x for x in range(1,50) if x%2 == 0]
+print(generate_evens())
